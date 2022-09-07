13e51f7d1dfc54c2e334256fb12fc3c9c53c3b44
deepu <achanta.deepu@gmail.com>
2022-09-07 20:08:08 +0530

    practised values types and operators

---
+++ value_types_operators/personal_details.js
@@ -0,0 +1,19 @@
+var name = "deepu";
+var email = "deepu@gmail.com";
+var bDate = 16;
+var bMonth = 04;
+var bYear = 1972;
+var gender = "Female";
+var maritalStatus = "Single";
+var phoneNumber = 9999999999;
+var jobDescription = "I am a UI Designer";
+var address = "Dubaii";
+
+console.log(jobDescription);
+console.log(name);
+console.log(email);
+console.log(bDate+"/"+bMonth+"/"+bYear);
+console.log(gender);
+console.log(maritalStatus);
+console.log(phoneNumber);
+console.log(address);
