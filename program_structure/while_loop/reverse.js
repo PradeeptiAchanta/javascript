0c9b5d87a0616f7dc9702e890a4920d42f22a555
deepu <achanta.deepu@gmail.com>
2022-09-16 15:17:45 +0530

    reverse_triangle.js

---
+++ program_structure/while_loop/reverse.js
@@ -0,0 +1,15 @@
+let number = 8;
+let hash = "#";
+while (number >=1) {
+  let temp = number;
+  let result = "";
+
+  while(temp>=1){
+
+  result = result+ hash
+  temp = temp -1;
+  
+  }
+  console.log(result);
+  number = number -1;
+}
