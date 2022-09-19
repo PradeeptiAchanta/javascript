4913a53d260902aaf924804911ca1af12d98f596
deepu <achanta.deepu@gmail.com>
2022-09-19 18:11:14 +0530

    tables

---
+++ program_structure/for_loop/leap_year.js
@@ -0,0 +1,6 @@
+var leapyear(year);
+for(year%100==,year%400==,year%4==){
+}
+console.log(leapyear(2014));
+console.log(leapyear(2026));
+console.log(leapyear(2000));
---
+++ program_structure/while_loop/tables.js
@@ -0,0 +1,10 @@
+var table = 2;
+while(table<=10){
+  var number = 1;
+ while(number<=10){
+   result=table*number;
+   console.log(result);
+   number += 1;
+ }
+table+= 1;
+}
